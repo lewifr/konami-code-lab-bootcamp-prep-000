5402c05839a8460dda6bb636a68fb0dd394cacdd
Francis Lewis <francis.lewis@me.com>
2019-04-14 11:40:08 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -31,10 +31,6 @@ function init() {
   document.body.addEventListener('keydown', function (e) {alert('yo')})
 
   document.body.addEventListener('keydown', function(e) {
-  if (e.key === "g") {
-    return e.preventDefault()
-  } else {
-    console.log(e.key)
-  }
+  alert('yo2')
 });
 }
