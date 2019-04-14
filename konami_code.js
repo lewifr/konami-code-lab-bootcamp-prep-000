d17781d37e8cc9ba1612d47754b27d68ae17fd9a
Francis Lewis <francis.lewis@me.com>
2019-04-14 11:25:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+    if (index === alphabet.length) {
+      alert('Konaaaaaamiiiiiiii !!!');
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
 function init() {
-  // your code here
+  document.addEventListener('keydown', onKeyDownHandler)
 }
